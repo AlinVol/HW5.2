5c824fa562194aa36a17f6bdfab623d47bc497ab
AlinVol <alina.voloshyna@gmail.com>
2024-05-29 23:17:19 +0200

    hw

---
+++ main.py
@@ -0,0 +1,24 @@
+while True:
+    number1 = int(input("Enter the first number: "))
+    number2 = int(input("Enter the second number: "))
+    action = input("Enter the action (+, -, *, /): ")
+
+    if action == '+':
+        result = number1 + number2
+    elif action == '-':
+        result = number1 - number2
+    elif action == '*':
+        result = number1 * number2
+    elif action == '/':
+        if number2 != 0:
+            result = number1 / number2
+        else:
+            result = "Error: Division by zero!"
+    else:
+        result = "Error: Invalid action!"
+
+    print("Result:", result)
+
+    continue_calculating = input("Do you want to perform another calculation? (yes/no): ").lower()
+    if continue_calculating != 'yes':
+        break
